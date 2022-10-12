1b3d12286ceadddb771f7fd504b46973206a743c
mae-meijuan-long <meijuan.long@miu.edu>
2022-10-12 14:31:17 -0500

    fix bugs
    fix bugs

--- W1D6JavascriptAnimation/ascii.js
+++ W1D6JavascriptAnimation/ascii.js
@@ -72,3 +72,24 @@ window.onload = function () {
                     clearInterval(resetTimer);
                 };
     };
+
+
+    x = 1;
+    var a = 5;
+    var b = 10;
+    var c = function(a,b,c){
+            console.log(x);
+            console.log(a);
+            var f = function(a,b,c){
+                b = a;
+                console.log(b);
+                b = c;
+                var x = 5;
+            }
+            f(a,b,c);
+            console.log(b);
+            var x = 10;
+    }
+    c(8,9,10);
+    console.log(b);
+    console.log(x);
