5bcf96b0021819fb7e78496b10db58b9ff9ff1be
mae-meijuan-long <meijuan.long@miu.edu>
2022-10-25 10:26:09 -0500

    quiz
    quiz

--- exersize_quiz_1006/quiz1024.js
+++ exersize_quiz_1006/quiz1024.js
@@ -2,23 +2,33 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.set('view engine', 'ejs');
-console.log("path==>"+path.join(__dirname, "ejs"));
 app.set('views', path.join(__dirname, "ejs"));
+const session = require('express-session');
+app.use(session({
+    secret: 'someBigSecret',
+    resave: true,
+    saveUninitialized: true
+}));
 
-
-let list = [];
 app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    if(!req.session.list){
+           req.session.list = [];
+      }
+    next();
+});
 
 app.get('/', (req, res) => {
    res.render("list", {
-      list: list,
+      list: req.session.list,
    });
 });
 app.get('/add', (req, res) => {
     res.render("add");
 });
 app.post('/add', (req, res) => {
-    list.push(req.body.item);
+    req.session.list.push(req.body.item);
     res.redirect(303, "/");
 });
 
